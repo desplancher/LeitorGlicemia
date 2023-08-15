b34ec7205cd5a9f609b9ebc081555dd3a58007af
João Desplancher Neto <joaodesplancher@gmail.com>
2023-08-15 20:04:18 -0300

    first commit

---
+++ src/main/java/PackageGlicemia/Exame.java
@@ -0,0 +1,40 @@
+
+package PackageGlicemia;
+
+
+public abstract class Exame {
+    private String nomePaciente;
+    private String tipoSanguineo;
+    private int anoNascimento;
+
+    public String getNomePaciente() {
+        return nomePaciente;
+    }
+
+    public void setNomePaciente(String nomePaciente) {
+        this.nomePaciente = nomePaciente;
+    }
+
+    public String getTipoSanguineo() {
+        return tipoSanguineo;
+    }
+
+    public void setTipoSanguineo(String tipoSanguineo) {
+        this.tipoSanguineo = tipoSanguineo;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+    
+    public abstract String classificarResultado();
+    
+    public abstract String mostrarResultado();
+    
+    public abstract String cadastrarExame();
+    
+}
---
+++ src/main/java/PackageGlicemia/Glicemia.java
@@ -0,0 +1,19 @@
+
+package PackageGlicemia;
+
+public class Glicemia {
+    private double valorGlicose;
+
+    public double getValorGlicose() {
+        return valorGlicose;
+    }
+
+    public void setValorGlicose(double valorGlicose) {
+        this.valorGlicose = valorGlicose;
+    }
+    
+    //public String classificarResultado(){
+    
+    
+   // }
+}
